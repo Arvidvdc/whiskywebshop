9d188653b0808bdbe4ff66efee2a59b36ed480c2
EdwinBoersema <edwinboersema@gmail.com>
2019-11-20 10:40:16 +0100

    Add sort butting, sorting articles by price asc.

--- public/JS/javascript.js
+++ public/JS/javascript.js
@@ -1,6 +1,32 @@
-const articles=document.querySelectorAll("div[data-price]");
+// defining var article for later use
+var articles = "";
 
-function sortPrice() {
+window.onload = () => {
+    getArticles();
+    handleSorting();
+};
 
+// gets all articles currently in the DOM
+function getArticles() {
+    articles = $("div[data-price]");
 }
 
+// Handles all sorting functions
+function handleSorting() {
+    sortByPrice();
+}
+
+// Sorts items based on price low > high
+function sortByPrice() {
+    $("#sortPrice").on("click", (event) => {
+        let sortedA = articles;
+
+        sortedA.sort((a, b) => {
+            let aVal = parseInt(a.getAttribute("data-price"));
+            let bVal = parseInt(b.getAttribute("data-price"));
+            return aVal - bVal;
+        });
+        $("#articles").append(sortedA);
+        event.preventDefault
+    });
+}
