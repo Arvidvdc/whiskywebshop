5522b6d44c3dfeef9e7c066a7a7a48653905cb4f
BennieKoops <bennie.koops@gmail.com>
2019-11-22 14:32:37 +0100

    Created init func and async synching function

--- public/JS/shopping-cart.js
+++ public/JS/shopping-cart.js
@@ -3,7 +3,20 @@ const cart = {
     // unique key to identify the cart
     Key: "123456",
     order: []
-   
+    // function to start on page load that checks if a cart already exists
+    init() {
+        let existing-content = localStorage.getItem(cart.Key);
+        if (existing-content) {
+            cart.order = JSON.parse(existing-content);
+        } else {
+            cart.sync();
+        }
+    },
+    // function to synchronize cart to localstorage
+    async sync() {
+        let current = JSON.stringify(cart.order);
+        await localStorage.setItem(cart.Key, current);
+    },
 }
 // array to collect items for shoppingcart
 
