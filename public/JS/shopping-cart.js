b12262402223c9a68c0be4fc3b3925be1a76b7c0
BennieKoops <bennie.koops@gmail.com>
2019-11-22 14:49:56 +0100

    Created function to find product in order array

--- public/JS/shopping-cart.js
+++ public/JS/shopping-cart.js
@@ -2,11 +2,11 @@
 const cart = {
     // unique key to identify the cart
     Key: "123456",
-    order: []
+    order: [{id:1}],
     // function to start on page load that checks if a cart already exists
     init() {
-        let existing-content = localStorage.getItem(cart.Key);
-        if (existing-content) {
+        let existingContent = localStorage.getItem(cart.Key);
+        if (existingContent) {
             cart.order = JSON.parse(existing-content);
         } else {
             cart.sync();
@@ -17,6 +17,20 @@ const cart = {
         let current = JSON.stringify(cart.order);
         await localStorage.setItem(cart.Key, current);
     },
+    // function to find product in cart object
+    find(id) {
+        // create a true statement if product exists in order array
+        let found = cart.order.filter(item => {
+            if(item.id == id){
+                return true;
+            }
+        });
+        console.log(found + " " + found[0]);
+        // return found product
+        if (found && found[0]){
+            return found[0];
+        }
+    }
 }
 // array to collect items for shoppingcart
 
