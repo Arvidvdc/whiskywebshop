5a18781f391ea817627796fd83f364ea4644205d
BennieKoops <56158309+BennieKoops@users.noreply.github.com>
2019-11-22 09:57:44 +0100

    Created function that adds object to shopping cart array

--- controllers/products.js
+++ controllers/products.js
@@ -19,3 +19,34 @@ exports.new_post = (req, res) => {
         }
     });
 }
+
+// array to collect items for shoppingcart
+var shoppingCartArray = [];
+
+// function to add items to shoppingcart array
+function addItem(name, price, ammount) {
+    if (ammount == 0) {
+        ammount = 1;
+    }
+    let newOrder = {
+        name: name,
+        price: price,
+        ammount: ammount
+    }
+    for
+    shoppingCartArray.forEach(item => {
+        var exists = Object.keys(item).some((i) => {
+            return item[i] === name;
+        });
+        console.log(exists);
+        return exists;
+    });
+    if (exists) {
+        console.log('newOrder ' + newOrder.ammount);
+        console.log('item ' + item.ammount);
+        item.ammount += newOrder.ammount;
+        return shoppingCartArray;
+    }
+    shoppingCartArray.push(newOrder);
+    return shoppingCartArray;
+}
