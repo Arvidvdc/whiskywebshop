51d52940ea3131d93b6965e8d0b88017c434dc7c
Arvid van de Crommert <arvidvdc@gmail.com>
2019-11-25 08:44:35 +0100

    Image available on shopping-cart.

--- controllers/shopping-cart.js
+++ controllers/shopping-cart.js
@@ -1,6 +1,6 @@
 
 // SHOW
 exports.show = (req,res) => {
-    let order=[{id: "qweqwfgergthtry", name: "Jameson", price: 10.50, amount:1},{id: "123123456", name: "Jack Daniels", price: 5.25, amount:2},{id: "ddsf027n32", name: "Johnny Walker Black Label", price: 19.50, amount:1}]
+    let order=[{id: "qweqwfgergthtry", name: "Jameson", price: 10.50, amount:1, image: "https://cdn.pixabay.com/photo/2017/11/12/15/14/whiskey-2942734_960_720.jpg"},{id: "123123456", name: "Jack Daniels", price: 5.25, amount:2, image: "https://cdn.pixabay.com/photo/2018/03/04/09/26/drink-3197547_960_720.jpg"},{id: "ddsf027n32", name: "Johnny Walker Red Label", price: 19.50, amount:1, image: "https://www.totalwine.com/dynamic/490x/media/sys_master/twmmedia/h95/hd8/11785453305886.png"}]
     res.render("./shopping-cart/show.ejs", {order: order});
 }
