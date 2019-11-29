4318118726030f145e4917adff2109115e5147fc
EdwinBoersema <edwinboersema@gmail.com>
2019-11-29 10:33:55 +0100

    Changed sendOrder to use localStorage, changed var names to articles.

--- public/JS/create-order.js
+++ public/JS/create-order.js
@@ -5,38 +5,38 @@ window.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-const testarticles = [
-    {
-        id: "5ddbdb00acfc8f4f145a138e",
-        amount: 2
-    },
-    {
-        id: "5ddbdb00acfc8f4f145a138f",
-        amount: 3
-    },
-    {
-        id: "5ddbdb00acfc8f4f145a1390",
-        amount: 1
-    },
-    {
-        id: "5ddbdb00acfc8f4f145a1391",
-        amount: 2
-    },
-    {
-        id: "5ddbdb00acfc8f4f145a1392",
-        amount: 5
-    }
-]
+// const testarticles = [
+//     {
+//         id: "5ddbdb00acfc8f4f145a138e",
+//         amount: 2
+//     },
+//     {
+//         id: "5ddbdb00acfc8f4f145a138f",
+//         amount: 3
+//     },
+//     {
+//         id: "5ddbdb00acfc8f4f145a1390",
+//         amount: 1
+//     },
+//     {
+//         id: "5ddbdb00acfc8f4f145a1391",
+//         amount: 2
+//     },
+//     {
+//         id: "5ddbdb00acfc8f4f145a1392",
+//         amount: 5
+//     }
+// ]
 
 async function sendOrder() {
     // get products and address from Storage
     let order = [];
     let addressData = JSON.parse(sessionStorage.getItem(address.key));
     order.push(addressData);
-    // let orderData = JSON.parse(localStorage.getItem(cart.Key));
-    let products = { products: [] };
-    products.products = testarticles;
-    order.push(products);
+    let articleData = JSON.parse(localStorage.getItem(cart.Key));
+    let articles = { articles: [] };
+    articles.articles = articleData;
+    order.push(articles);
 
     // fetch the data to the server
     await fetch('http://localhost:3000/bestellen/betalen', {
@@ -48,7 +48,6 @@ async function sendOrder() {
         mode: "cors",
         // redirect to the returned url
     }).then((response => {
-        // window.location.replace(response.url);
-        window.location.replace("google.nl");
+        window.location.replace(response.url);
     }));
 }
