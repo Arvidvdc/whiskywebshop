07ee5b11c43836e183e1b9ec99fdc675b8f2b35e
Arvid van de Crommert <arvidvdc@gmail.com>
2019-12-11 13:37:50 +0100

    Updated user model.

--- models/user.js
+++ models/user.js
@@ -5,7 +5,15 @@ const UserSchema = new mongoose.Schema({
     username: String,
     password: String,
     email: String,
-    role: String
+    role: String,
+    company: String,
+    firstname: String,
+    lastname: String,
+    zipcode: String,
+    nr: String,
+    phone: String,
+    street: String,
+    city: String 
 });
 
 UserSchema.plugin(passportLocalMongoose);
