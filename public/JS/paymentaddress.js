71a1f4215ee915d9c4d675bd799892344ed7f57d
EdwinBoersema <edwinboersema@gmail.com>
2019-12-12 12:51:26 +0100

    Deleted unused create-order.js, cleaned up paymentaddress.js .

--- public/JS/paymentaddress.js
+++ public/JS/paymentaddress.js
@@ -1,53 +1,7 @@
-// const address = {
-//     key: "987",
-//     contents: {},
-//     init() {
-//         // check for sessionStorage on this key
-//         let _address = sessionStorage.getItem(address.key);
-//         if (_address) {
-//             address.contents = JSON.parse(_address);
-//         } else {
-//             address.update();
-//         }
-//     },
-//     // get address
-//     getNewAddress() {
-//         address.contents = {
-//             address: {
-//                 company: document.getElementsByName("company")[0].value,
-//                 firstname: document.getElementsByName("firstname")[0].value,
-//                 lastname: document.getElementsByName("lastname")[0].value,
-//                 zipcode: document.getElementsByName("zipcode")[0].value,
-//                 nr: document.getElementsByName("nr")[0].value,
-//                 phone: document.getElementsByName("phone")[0].value,
-//                 street: document.getElementsByName("street")[0].value,
-//                 city: document.getElementsByName("city")[0].value
-//             }
-//         };
-//         address.update();
-//     },
-//     // put content to sessionStorage
-//     async update() {
-//         let newAddress = JSON.stringify(address.contents);
-//         await sessionStorage.setItem(address.key, newAddress);
-//     },
-//     logContent() {
-//         console.log(address.contents);
-//     }
-// }
-
-// Add address function
-// function addAddress() {
-//     document.getElementById("addressBtn").addEventListener('click', (event) => {
-//         // address.getNewAddress();
-//         sendOrder();
-//         event.preventDefault();
-//     });
-// }
-
 async function sendOrder() {
-    // get products and address from Storage
+    // get products from storage and address from form
     let order = [];
+
     let addressData = {
         address: {
             company: document.getElementsByName("company")[0].value,
@@ -61,13 +15,14 @@ async function sendOrder() {
         }
     };
     order.push(addressData);
+
     let articleData = JSON.parse(localStorage.getItem(cart.Key));
     let articles = { articles: [] };
     articles.articles = articleData;
     order.push(articles);
-    console.log(order);
+
     let url = window.location.href;
-    console.log(url);
+
     // fetch the data to the server
     await fetch(url, {
         method: "POST",
@@ -81,12 +36,10 @@ async function sendOrder() {
     });
 }
 
-// initialize address on load and assign eventlistener to the button.
+// add eventlistener
 window.addEventListener("DOMContentLoaded", () => {
-    // address.init();
     if (document.getElementById("addressBtn")) {
         document.getElementById("addressBtn").addEventListener('click', (event) => {
-            // address.getNewAddress();
             sendOrder();
             event.preventDefault();
         });
